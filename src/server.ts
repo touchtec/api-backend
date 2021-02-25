5ea1dfd913138e9b86cd189ad365b95a6e545f60
touch <touchtechnologia@gmail.com>
2021-02-25 17:18:30 -0300

    Repositório do TypeORM

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,13 @@
+import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
+
+import './database';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
-
-
-/*app.get('/', (request, response) => {
-    return response.json({ message : 'Hello Barber...'});
-})*/
 
 app.listen(4000, () => {
     console.log('Server started port on 4000!');

c0bedf85b8930246c6ccaee8788a26897aaebbf3
touch <touchtechnologia@gmail.com>
2021-02-26 12:08:55 -0300

    Criação de registros

--- src/database/migrations/1614336970300-CreateUsers.ts
+++ src/database/migrations/1614336970300-CreateUsers.ts
@@ -1,11 +1,11 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export default class CreateAppointments1614278139190 implements MigrationInterface {
+export default class CreateUsers1614336970300 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: 'appointments',
+                name: 'users',
                 columns: [
                     {
                         name: 'id',
@@ -14,14 +14,19 @@ export default class CreateAppointments1614278139190 implements MigrationInterfa
                         generationStrategy:'uuid',
                     },
                     {
-                        name: 'provider',
+                        name: 'name',
                         type: 'varchar',
                         isNullable: false,
                     },
                     {
-                        name: 'date',
-                        type: 'timestamp',
+                        name: 'email',
+                        type: 'varchar',
+                        isUnique: true,
                         isNullable: false,
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar',
                     },
                     {
                         name: 'created_at',
@@ -39,7 +44,9 @@ export default class CreateAppointments1614278139190 implements MigrationInterfa
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('appointments');
+        await queryRunner.dropTable('users');
     }
+
+    
 
 }

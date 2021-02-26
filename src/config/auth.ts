0cc038642f314519e727fb5a952215d54fec7257
touch <touchtechnologia@gmail.com>
2021-02-26 15:57:33 -0300

    Rotas autenticadas

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: '48e758fab8888fae531f5ed2c402a19d',
+        expiresIn: '1d',
+    },
+}
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,8 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
+
 import User from '../models/Users';
 
 interface RequestDTO {
@@ -9,6 +12,7 @@ interface RequestDTO {
 
 interface Response {
     user: User;
+    token: string;
 }
 
 class AuthenticateUserService {
@@ -27,8 +31,16 @@ class AuthenticateUserService {
             throw new Error('Incorret email/password combination');
         }
 
+        const { secret, expiresIn } = authConfig.jwt;
+
+        const token = sign({ }, secret, {
+            subject: user.id,
+            expiresIn,
+        });
+
         return {
             user,
+            token,
         };
 
     }

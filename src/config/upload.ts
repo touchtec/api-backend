a794104ac9674a62ff80dcb29e3b449d06799633
touch <touchtechnologia@gmail.com>
2021-03-01 14:25:59 -0300

    Lidando com erros - Final nível 2

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,11 +1,15 @@
 import patch from 'path';
 import crypto from 'crypto';
 import multer from 'multer';
-import { request } from 'express';
+
+const tmpFolder = patch.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
+
+    directory: tmpFolder,
+
     storage: multer.diskStorage({
-       destination: patch.resolve(__dirname, '..', '..', 'tmp'),
+       destination: tmpFolder ,
        filename(request, file, callback) {
            const fileHash = crypto.randomBytes(10).toString('hex');
            const fileName = `${fileHash}-${file.originalname}`;

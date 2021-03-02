adb185c4ae339d7e4a564e7abc9d6e30f2d811a0
touch <touchtechnologia@gmail.com>
2021-03-02 12:50:57 -0300

    Dependency Inversion Principle

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,9 +1,8 @@
 import { startOfHour } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
-import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import AppError from '@shared/errors/AppError';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface RequestDTO {
     provider_id: string;
@@ -11,24 +10,24 @@ interface RequestDTO {
 }
 
 class CreateAppointmentService {
+    constructor(private appointmentsRepository: IAppointmentsRepository){}
+
+
     public async execute({ date, provider_id }: RequestDTO): Promise<Appointment> {
-        const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
         const appointmentDate =  startOfHour(date);
 
-        const findAppointmentInSameDate = await appointmentsRepository.findByDate(appointmentDate);
+        const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(appointmentDate);
     
 
     if (findAppointmentInSameDate) {
         throw new AppError('This appointment is already booked');
     }
 
-    const appointment = appointmentsRepository.create({
+    const appointment = await this.appointmentsRepository.create({
         provider_id,
         date: appointmentDate,
     });
-
-    await appointmentsRepository.save(appointment);
 
     return appointment;
 

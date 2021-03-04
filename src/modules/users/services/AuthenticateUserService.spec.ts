d872515d19ae95276ec050bb87ec14c87419abf3
touch <touchtechnologia@gmail.com>
2021-03-04 17:08:38 -0300

    Nível 4 - Testes de autenticação

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -2,6 +2,7 @@ import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import AuthenticateUserService from '../services/AuthenticateUserService';
 import CreateUserService from '../services/CreateUserService';
+import AppError from '@shared/errors/AppError';
 
 describe('AuthenticateUser', () => {
     it('should be able to authenticate', async () => {
@@ -23,5 +24,38 @@ describe('AuthenticateUser', () => {
         });
 
         expect(response).toHaveProperty('token');
+       //expect(response.user).toEqual(user);
     });
+
+    it('should not be able to authenticate with non existing user', async () => {
+        const fakeUsersRepository = new FakeUsersRepository();
+        const fakeHashProvider = new FakeHashProvider();        
+        const authenticateUser = new AuthenticateUserService(fakeUsersRepository, fakeHashProvider);
+
+        expect(authenticateUser.execute({
+            email: 'john@exemplo.com',
+            password: '1234',
+        })).rejects.toBeInstanceOf(AppError);
+    });
+
+    it('should not be able to authenticate with wrong password', async () => {
+        const fakeUsersRepository = new FakeUsersRepository();
+        const fakeHashProvider = new FakeHashProvider();        
+
+        const createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider);
+        const authenticateUser = new AuthenticateUserService(fakeUsersRepository, fakeHashProvider);
+
+        await createUser.execute({
+            name: 'John Doe',
+            email: 'john@exemplo.com',
+            password: '1234',
+        });
+
+
+        expect(authenticateUser.execute({
+            email: 'john@exemplo.com',
+            password: 'wrong-password',
+        })).rejects.toBeInstanceOf(AppError);
+    });
+   
 });  
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,3 +1,4 @@
 import { container } from 'tsyringe';
 
 import '@modules/users/providers';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
